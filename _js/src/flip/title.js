d332af1720bb4b3fd3c1b5116c73604e41abb788
Becca Steinbrecher <beccasteinbrecher@gmail.com>
2026-09-14 14:55:54 -0600

    Update title.js

--- _js/src/flip/title.js
+++ _js/src/flip/title.js
@@ -12,6 +12,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 import { Observable, of, zip } from 'rxjs';
 import { tap, finalize, filter, map, switchMap } from 'rxjs/operators';
